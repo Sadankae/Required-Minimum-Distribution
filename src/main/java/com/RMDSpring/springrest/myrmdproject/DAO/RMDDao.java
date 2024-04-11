98492ff11535f32adef0253fb4993179cfb5d3dd
LAPTOP-171SEUGB\VIGNESH <vigneshsadankaelas1998@gmail.com>
2024-04-11 13:25:44 +0530

    Initial Commit

---
+++ src/main/java/com/RMDSpring/springrest/myrmdproject/DAO/RMDDao.java
@@ -0,0 +1,11 @@
+package com.RMDSpring.springrest.myrmdproject.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.RMDSpring.springrest.entities.Participant;
+
+@Repository
+public interface RMDDao extends JpaRepository<Participant, Long> {
+
+}
